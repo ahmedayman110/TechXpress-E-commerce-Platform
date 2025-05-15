139a317ed4f5a80fbb627191e0de8a89bbc3fa1f
Ahmed Ayman Abdulatty <113814774+ahmedayman110@users.noreply.github.com>
2025-05-15 06:28:19 +0300

    Delete Models directory

--- Models/JwtSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TechXpress.Models
-{
-    public class JwtSettings
-    {
-        public string SecretKey { get; set; }
-        public string Issuer { get; set; }
-        public string Audience { get; set; }
-    }
-}
